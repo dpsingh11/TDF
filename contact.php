e9403c741333b55688b293856d3734ae399b6f8b
dpsingh11 <singh.dprakash@gmail.com>
2016-12-26 02:51:00 +0530

    first commit for tdf

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php  require("header.php"); ?>
+<script type="text/javascript">
+	document.getElementById("acontact").className="active";
+</script>
+
+
+	<h1>Contact Us</h1>
+    <table>
+    
+	
+    <tr><td rowspan="8"><img src="ups/fl.jpg" style='height:100px; width:100px';></td><td>Name</td><td>:</td><td>Devesh P Singh</td></tr>
+    <tr><td rowspan="1">Contact No</td><td>:</td><td>7309456129</td></tr>
+    <tr><td rowspan="1">E-Mail Add</td><td>:</td><td>singh.dprakash@gmail.com</td></tr>
+   <tr><td rowspan="1">Website</td><td>:</td><td><a href="http://www.gupshupnow.com/">www.comingsoon.com</a></td></tr>
+    </table>
+    
+<?php require("footer.php"); ?>
---
+++ footer.php
@@ -0,0 +1,16 @@
+<div class="cleared"></div><div class="art-Footer">
+                    <div class="art-Footer-inner">
+                        <a href="#" class="art-rss-tag-icon" title="RSS"></a>
+                        <div class="art-Footer-text">
+                            <p><a href="contact.php">Contact Us</a> | <a href="terms.php">Terms of Use</a> 
+                                | <a href="prvs.php">Privacy Statement</a> | <a href="isuser.php">Online User</a><br />
+                                Copyright &copy; 2016 ---. All Rights Reserved.@BBAU_DCS_TEAM</p>
+                        </div>
+                    </div>
+                    <div class="art-Footer-background"></div>
+                </div>
+            </div>
+        </div>
+           
+</body>
+</html>
